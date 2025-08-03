53b0c95601bc084d5450836ff78fe893794e0ea7
style1027 <146694767+style1027@users.noreply.github.com>
2025-08-03 23:57:48 +0800

    Update MiSeaCoreBlocks.cs

--- MiSeaCoreBlocks.cs
+++ MiSeaCoreBlocks.cs
@@ -21,7 +21,7 @@ namespace MiSeaCore
             public static int Index = 408;
             public MiSeaFlatBlock()
             //Modified at 8: 37 on August 3rd, 2025. <<
-               : base("Textures/FlatBlocks/MiSeaFlatBlock")  //所有flat方块的贴图整一张512x512的图片上,和原版一样，贴图是32x32
+               : base("Textures/FlatBlocks/MiSeaFlatBlock")  //所有flat方块的贴图整一张512x512的图片上,和原版一样，贴图是16x16
                // >>
             {
                 DefaultCategory = "MiSeaFlatBlock";
