9d76e878443c5e175303adddeed3150ef04a6b87
Mxher07 <2360881635@qq.com>
2025-08-04 17:05:08 +0800

    111

--- obj/Debug/net9.0/OTI.AssemblyInfo.cs
+++ obj/Debug/net9.0/OTI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73c459606c83b0878bb2193baf915eadc26dc91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8541777dd2bf214241e10273391155aab8cd593c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
